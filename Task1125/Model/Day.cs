d35ceb203448600a4cdbdb56c33929a067e9b0c6
Данил Тимофеев <danil2006timofeev@gmail.com>
2024-10-03 21:44:37 +1000

    Добавьте файлы проекта.

---
+++ Task1125/Model/Day.cs
@@ -0,0 +1,10 @@
+﻿//это тоже модель, как и лекции
+namespace Task1125.Model
+{
+    public class Day
+    {
+        public string Title {  get; set; }
+        public string Group { get; set; }
+        public List<Lesson> Lessons { get; set; }
+    }
+}
---
+++ Task1125/Model/Lesson.cs
@@ -0,0 +1,16 @@
+﻿//Это модель данных, тут ничего менять не нужно
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Task1125.Model
+{
+    public class Lesson
+    {
+        public string Name { get; set; }
+        public string Content { get; set; }
+
+        [NotMapped]
+        public bool IsLection { get; set; }
+        public string Type { get => IsLection ? "лекция" : "практика"; }
+    }
+}
